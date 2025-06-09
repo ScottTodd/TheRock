be8c2fd303436e0979f24511f1c84b85de3d5900
Mika Laitio <lamikr@gmail.com>
2025-06-09 11:09:50 -0700

    Updated rockbuilder logic and config options
    - Update the default rocm_home default seach path after the code
       was moved to another directory: ../../dist/build/rocm
     - ROCM_HOME env variable can be used to specify a different location
       for the rocm-sdk that will be used by the rockbuilder
    - Use patches from externa-builds/pytorch/patches folder instead of forking it
    - Support building pytorch, pytorch audio and vision also on windows
      without requiring  a bash prompt
    - Add option to specify environment variables
      - Environment variables are set at the time when project phases are executed
        and then reset back to previous values or removed after the project has been build
        (env_common, env_linux, env_windows)
    - git am fix for hipify run on windows
        - Hipified patches could fail to apply with command "git am"
          if the hipify command was executed on dos-prompt instead of using a bash-prompt.
        - Reason for the failure was that the original hipify patches expected
          that they would be applied to files having Linux line ending.
          When hipify is executed on dos-prompt they wil however have a dos line ending
          and that caused the patched to file.
        - This is fixed with '--ignore-whitespace' flag for git am.
    - Update messages printed out during the build steps
    - Improve code checkout and hipify phases
    - Add separate/optional hipify_cmd to config files instead of using
      the config-command to execute the hipify because we need to do in the
      background do additional things during the hipify phase
          a. If hipify command is specified in config file
            - run hipify command to scan and convert files to hip/rocm format
            - commit code changes made by hipify command to own commit
          b. If patches/hipified directory exist
            - apply patches from that directory even if the hipify_cmd is not specified
    - Add optional pre_config and post_config command phases
    - Perform "git reset --hard" automatically before trying to execute
      the git checkout command.
        - This fixes git checkout failures in cases where the project directory
          contains modifications to files that have not been committed.
          This can happen for the files for example if the previously executed build
          command has modified them
    - Add error checks, error message printouts and process termination
      if the command execution failed:
        - added support for commands having multiple lines
        - all commands executed can now contain from more than
          one line (earlier restricted for the build command)
    - Added support for specifying cmake config commands:
        - If cmake config options are specified, we will execue cmake-config,
          build and install steps automatically in addition of executing other
          config/build/install commands that can also be present in the config-file.
          For example the amd-smi project build and install will rquire the
          usage of both the cmake specific configure, build and install steps and
          then the execution of 'pip install'-command
    - Added more projects to test the project config and build options
    
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- experimental/rockbuilder/lib_python/project_builder.py
+++ experimental/rockbuilder/lib_python/project_builder.py
@@ -11,6 +11,7 @@ class RockProjectBuilder(configparser.ConfigParser):
     def __init__(self, rock_builder_root_dir, project_name):
         super(RockProjectBuilder, self).__init__(allow_no_value=True)
 
+        self.is_posix = not any(platform.win32_ver())
         self.project_name = project_name
         self.cfg_file_path = (
             Path(rock_builder_root_dir) / "projects" / f"{project_name}.cfg"
@@ -19,22 +20,75 @@ class RockProjectBuilder(configparser.ConfigParser):
             self.read(self.cfg_file_path)
         else:
             raise ValueError(
-                "Could not find the configuration file: " + str(self.cfg_file_path)
+                "Could not find the configuration file: "
+                + self.cfg_file_path.as_posix()
             )
         self.repo_url = self.get("project_info", "repo_url")
         self.project_version = self.get("project_info", "version")
+
+        # environment setup can have common and os-specific sections that needs to be appended together
+        self.skip_on_os = None
+        try:
+            if self.is_posix:
+                self.skip_on_os = self.get("project_info", "skip_linux")
+            else:
+                self.skip_on_os = self.get("project_info", "skip_windows")
+        except Exception as ex1:
+            pass
+        self.env_setup_cmd = None
+        try:
+            value = self.get("project_info", "env_common")
+            self.env_setup_cmd = list(
+                filter(None, (x.strip() for x in value.splitlines()))
+            )
+        except:
+            pass
+        try:
+            if self.is_posix:
+                value = self.get("project_info", "env_linux")
+                temp_env_list = list(
+                    filter(None, (x.strip() for x in value.splitlines()))
+                )
+            else:
+                value = self.get("project_info", "env_windows")
+                temp_env_list = list(
+                    filter(None, (x.strip() for x in value.splitlines()))
+                )
+            if self.env_setup_cmd:
+                self.env_setup_cmd.extend(temp_env_list)
+            else:
+                self.env_setup_cmd = temp_env_list
+        except:
+            pass
+        try:
+            self.init_cmd = self.get("project_info", "init_cmd")
+        except:
+            self.init_cmd = None
         try:
             self.clean_cmd = self.get("project_info", "clean_cmd")
         except:
             self.clean_cmd = None
         try:
-            self.configure_cmd = self.get("project_info", "configure_cmd")
+            self.hipify_cmd = self.get("project_info", "hipify_cmd")
         except:
-            self.configure_cmd = None
+            self.hipify_cmd = None
         try:
+            self.pre_config_cmd = self.get("project_info", "pre_config_cmd")
+        except:
+            self.pre_config_cmd = None
+        try:
+            self.config_cmd = self.get("project_info", "config_cmd")
+        except:
+            self.config_cmd = None
+        try:
+            self.post_config_cmd = self.get("project_info", "post_config_cmd")
+        except:
+            self.post_config_cmd = None
+        try:
+            build_cmd = None
             is_dos = any(platform.win32_ver())
-            if is_dos and self.has_option("project_info", "build_cmd_dos"):
-                self.build_cmd = self.get("project_info", "build_cmd_dos")
+            if is_dos and self.has_option("project_info", "build_cmd_windows"):
+                self.build_cmd = self.get("project_info", "build_cmd_windows")
             else:
                 self.build_cmd = self.get("project_info", "build_cmd")
             print("Build_cmd: ------------")
@@ -44,9 +98,17 @@ class RockProjectBuilder(configparser.ConfigParser):
             print(ex1)
             self.build_cmd = None
         try:
+            self.cmake_config = self.get("project_info", "cmake_config")
+        except:
+            self.cmake_config = None
+        try:
             self.install_cmd = self.get("project_info", "install_cmd")
         except:
             self.install_cmd = None
+        try:
+            self.post_install_cmd = self.get("project_info", "post_install_cmd")
+        except:
+            self.post_install_cmd = None
         self.project_root_dir_path = Path(rock_builder_root_dir)
         self.project_src_dir_path = (
             Path(rock_builder_root_dir) / "src_projects" / self.project_name
@@ -54,76 +116,127 @@ class RockProjectBuilder(configparser.ConfigParser):
         self.project_build_dir_path = (
             Path(rock_builder_root_dir) / "builddir" / self.project_name
         )
-        self.patch_dir_path = (
-            Path(rock_builder_root_dir) / "patches" / self.project_name
+        try:
+            self.cmd_execution_dir = self.get("project_info", "cmd_exec_dir")
+        except:
+            # default value if not specified in the config-file
+            self.cmd_execution_dir = self.project_src_dir_path
+        self.project_patch_dir_root = (
+            Path(rock_builder_root_dir)
+            / "../../external-builds/pytorch/patches"
+            / self.project_name
+        )
+        self.project_patch_dir_root = self.project_patch_dir_root.resolve()
+        self.project_repo = RockProjectRepo(
+            self.project_name,
+            self.project_root_dir_path,
+            self.project_src_dir_path,
+            self.project_build_dir_path,
+            self.cmd_execution_dir,
+            self.repo_url,
+            self.project_version,
+            self.project_patch_dir_root,
         )
 
     # printout project builder specific info for logging and debug purposes
-    def printout(self):
-        print("Project: ---------------")
-        print("    Name: " + self.project_name)
-        print("    Config path: " + str(self.cfg_file_path))
+    def printout(self, phase):
+        print("Project build phase " + phase + ": -----")
+        print("    Project_name: " + self.project_name)
+        print("    Config_path: " + self.cfg_file_path.as_posix())
         print("    Version:     " + self.project_version)
-        print("    Source_dir:  " + str(self.project_build_dir_path))
-        print("    Patch_dir:   " + str(self.patch_dir_path))
-        print("    Build dir:   " + self.project_version)
+        print("    Source_dir:  " + self.project_src_dir_path.as_posix())
+        print("    Patch_dir:   " + self.project_patch_dir_root.as_posix())
+        print("    Build_dir:   " + self.project_build_dir_path.as_posix())
         print("------------------------")
 
-    def checkout(self):
-        project_repo = RockProjectRepo(
-            self.project_name,
-            self.project_root_dir_path,
-            self.project_src_dir_path,
-            self.project_build_dir_path,
-            self.repo_url,
-            self.project_version,
-        )
-        project_repo.do_checkout()
+    def printout_error_and_terminate(self, phase):
+        self.printout(phase)
+        print(phase + " failed")
+        sys.exit(1)
+
+    # check whether operations should be skipped on current operating system
+    def check_skip_on_os(self):
+        ret = True
+        if (self.skip_on_os is None) or (
+            (self.skip_on_os != "1")
+            and (str(self.skip_on_os).casefold() != str("y").casefold())
+            and (str(self.skip_on_os).casefold() != str("yes").casefold())
+            and (str(self.skip_on_os).casefold() != str("on").casefold())
+        ):
+            ret = False
+        return ret
+
+    def do_env_setup(self):
+        res = self.project_repo.do_env_setup(self.env_setup_cmd)
+        if not res:
+            self.printout_error_and_terminate("env_setup")
+
+    def undo_env_setup(self):
+        res = self.project_repo.undo_env_setup(self.env_setup_cmd)
+        if not res:
+            self.printout_error_and_terminate("undo_env_setup")
+
+    def init(self):
+        res = self.project_repo.do_init(self.init_cmd)
+        if not res:
+            self.printout_error_and_terminate("init")
 
     def clean(self):
-        project_repo = RockProjectRepo(
-            self.project_name,
-            self.project_root_dir_path,
-            self.project_src_dir_path,
-            self.project_build_dir_path,
-            self.repo_url,
-            self.project_version,
-        )
-        project_repo.do_clean(self.clean_cmd)
+        res = self.project_repo.do_clean(self.clean_cmd)
+        if not res:
+            self.printout_error_and_terminate("clean")
 
-    def configure(self):
-        project_repo = RockProjectRepo(
-            self.project_name,
-            self.project_root_dir_path,
-            self.project_src_dir_path,
-            self.project_build_dir_path,
-            self.repo_url,
-            self.project_version,
-        )
-        project_repo.do_configure(self.configure_cmd)
+    def checkout(self):
+        res = self.project_repo.do_checkout()
+        if not res:
+            self.printout_error_and_terminate("checkout")
+
+    def hipify(self):
+        res = self.project_repo.do_hipify(self.hipify_cmd)
+        if not res:
+            self.printout_error_and_terminate("hipify")
+
+    def pre_config(self):
+        res = self.project_repo.do_pre_config(self.pre_config_cmd)
+        if not res:
+            self.printout_error_and_terminate("pre_config")
+
+    def config(self):
+        res = True
+        if self.cmake_config:
+            res = self.project_repo.do_cmake_config(self.cmake_config)
+        if res:
+            res = self.project_repo.do_config(self.config_cmd)
+        if not res:
+            self.printout_error_and_terminate("config")
+
+    def post_config(self):
+        res = self.project_repo.do_post_config(self.post_config_cmd)
+        if not res:
+            self.printout_error_and_terminate("post_config")
 
     def build(self):
-        if self.build_cmd is not None:
-            project_repo = RockProjectRepo(
-                self.project_name,
-                self.project_root_dir_path,
-                self.project_src_dir_path,
-                self.project_build_dir_path,
-                self.repo_url,
-                self.project_version,
-            )
-            project_repo.do_build(self.build_cmd)
+        res = True
+        if self.cmake_config:
+            res = self.project_repo.do_cmake_build(self.cmake_config)
+        if res:
+            res = self.project_repo.do_build(self.build_cmd)
+        if not res:
+            self.printout_error_and_terminate("build")
 
     def install(self):
-        project_repo = RockProjectRepo(
-            self.project_name,
-            self.project_root_dir_path,
-            self.project_src_dir_path,
-            self.project_build_dir_path,
-            self.repo_url,
-            self.project_version,
-        )
-        project_repo.do_install(self.install_cmd)
+        res = True
+        if self.cmake_config:
+            res = self.project_repo.do_cmake_install(self.cmake_config)
+        if res:
+            res = self.project_repo.do_install(self.install_cmd)
+        if not res:
+            self.printout_error_and_terminate("install")
+
+    def post_install(self):
+        res = self.project_repo.do_post_install(self.post_install_cmd)
+        if not res:
+            self.printout_error_and_terminate("post_install")
 
 
 class RockExternalProjectListManager(configparser.ConfigParser):
