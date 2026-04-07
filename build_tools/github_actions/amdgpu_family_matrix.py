553ad755952546df11d20a1e5f4c83165cd85242
Geo Min <geomin12@amd.com>
2026-04-07 09:22:54 -0700

    [ci] Re-enabling gfx90a runners (#4360)
    We have gfx90a runners from cirrascale. These labels are set up with
    actions-prolense!
    
    Tests here:
    https://github.com/ROCm/TheRock/actions/runs/24091222142/job/70278066748

--- build_tools/github_actions/amdgpu_family_matrix.py
+++ build_tools/github_actions/amdgpu_family_matrix.py
@@ -216,13 +216,11 @@ amdgpu_family_info_matrix_nightly = {
     },
     "gfx90a": {
         "linux": {
-            # Label is linux-gfx90a-gpu-rocm
-            # Downtime in 3/17/26 - 3/18/26 for maintenance
-            "test-runs-on": "",
+            "test-runs-on": "linux-gfx90a-gpu-rocm",
             "family": "gfx90a",
             "fetch-gfx-targets": ["gfx90a"],
-            "sanity_check_only_for_family": True,
             "build_variants": ["release"],
+            "nightly_check_only_for_family": True,
         },
         "windows": {
             "test-runs-on": "",
